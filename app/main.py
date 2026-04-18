ce8e756c801ac996ebc3f563ec7f195acdc8744c
RenataEmy <renata.emy2012@gmail.com>
2026-04-18 18:11:05 -0300

    chore: add project structure

---
+++ app/main.py
@@ -0,0 +1,9 @@
+# app/main.py
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Ateliê Digital - Orders API",
+    description="Microsserviço de gerenciamento de pedidos.",
+    version="0.1.0",
+)
+ 
