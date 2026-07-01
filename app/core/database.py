508bd4b90b7579fdf8f5809ae01b4d70e139bde0
Aroldo <aroldo.caval@gmail.com>
2026-07-01 20:32:12 -0300

    feat: unified composes with network

--- app/core/database.py
+++ app/core/database.py
@@ -8,7 +8,12 @@ from sqlalchemy.sql import func
 from app.core.config import settings
 
 engine = create_async_engine(
-    settings.DATABASE_URL
+    settings.DATABASE_URL,
+    connect_args={
+    "server_settings": {
+        "search_path": settings.DATABASE_SCHEMA
+    }
+    }
 )
 
 
